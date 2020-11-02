998f2228289493b37f9c3e86061b31cd7d24f689
francoisstiennon <francois.stiennon@gmail.com>
2020-11-02 19:17:17 +0000

    Instantiate a tracer and register it in GlobalTracer

--- src/Application.kt
+++ src/Application.kt
@@ -1,18 +1,37 @@
 package com.github.fstien
 
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.zopa.ktor.opentracing.ThreadContextElementScopeManager
+import io.jaegertracing.Configuration
+import io.jaegertracing.internal.samplers.ConstSampler
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.http.*
 import io.ktor.jackson.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.opentracing.util.GlobalTracer
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    val tracer = Configuration("tracing-example")
+        .withSampler(Configuration.SamplerConfiguration.fromEnv()
+            .withType(ConstSampler.TYPE)
+            .withParam(1))
+        .withReporter(Configuration.ReporterConfiguration.fromEnv()
+            .withLogSpans(true)
+            .withSender(
+                Configuration.SenderConfiguration()
+                    .withAgentHost("localhost")
+                    .withAgentPort(6831))).tracerBuilder
+        .withScopeManager(ThreadContextElementScopeManager())
+        .build()
+
+    GlobalTracer.registerIfAbsent(tracer)
 
     install(ContentNegotiation) {
         jackson {
